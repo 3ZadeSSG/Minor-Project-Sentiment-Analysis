4a3114c90cb2170fc241e01d7d21ed3542de20c7
Manu Gond <manugond45@gmail.com>
2019-03-04 00:52:27 +0530

    initial commit

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 from flask import Flask,render_template,request
 app = Flask(__name__)
+# commons.py contains all methods to load the model from checkpoint and predict the sentiment
 from commons import *
 
 @app.route("/",methods=['GET','POST'])
 def hello():
 	if request.method=='GET':
+		# if GET method then render the homepage where data can be entered
 		return render_template('index.html')
 	if request.method=='POST':
+		# if POST method then first call the prediction method to get result 
+		# then render the result template and put result there
 		file=request.form['file']
-		#print("\n\t\tRequest RECEIVED")
 		predicted_result=getSentimentPredictionResult(file)
 		return render_template('result.html',sentiment=predicted_result)
 		
--- commons.py
+++ commons.py
@@ -4,141 +4,94 @@ from string import punctuation
 from collections import Counter
 import torch 
 import numpy as np
-
-def pad_features(reviews_ints, seq_length):
-    features = np.zeros((len(reviews_ints), seq_length), dtype=int)
-    for i, row in enumerate(reviews_ints):
-        features[i, -len(row):] = np.array(row)[:seq_length]
-    return features
-
-def tokenize_review(test_review,vocab_to_int):
-    test_review = test_review.lower()
-    test_text = ''.join([c for c in test_review if c not in punctuation])
-    test_words = test_text.split()
-    test_ints = []
-    sample=[]
-    for word in test_words:
-        if word in vocab_to_int:
-            sample.append(word)
-    test_ints.append([vocab_to_int[word] for word in sample])
-    return test_ints
-
-
-def predict(net, test_review, vocab_to_int,sequence_length=200):
-    net.eval()
-    test_ints = tokenize_review(test_review,vocab_to_int)
-    seq_length=sequence_length
-    features = pad_features(test_ints, seq_length)
-    feature_tensor = torch.from_numpy(features)
-    batch_size = feature_tensor.size(0)
-    h = net.init_hidden(batch_size)
-    output, h = net(feature_tensor.long(), h)
-    pred = torch.round(output.squeeze()) 
-    resultString='Prediction value: {:.6f}\t'.format(output.item())
-    if(pred.item()==1):
-        resultString=resultString+"Positive statement detected!"
+#########################################################################
+def featuresPadding(sentence,sequenceLength):
+  features=np.zeros((len(sentence),sequenceLength),dtype=int)
+  for i,row in enumerate(sentence):
+    features[i,-len(row):]=np.array(row)[:sequenceLength]
+  return features
+############################################################################
+def tokenize_sentence(sentence,vocab_to_int):
+  sentence=sentence.lower()
+  sentence=''.join([letter for letter in sentence if letter not in punctuation])
+  test_words=sentence.split()
+  tokens=[]
+  sample=[]
+  for word in test_words:
+      if word in vocab_to_int:
+          sample.append(word)
+  tokens.append([vocab_to_int[word] for word in sample])
+  return tokens
+##############################################################################
+def predict(model,sentence, vocab_to_int,sequence_length=200):
+    test_ints=tokenize_sentence(sentence,vocab_to_int)
+    features=featuresPadding(test_ints,sequence_length)
+    feature_tensor=torch.from_numpy(features)
+    batch_size=feature_tensor.size(0)
+    
+    hidden_state=model.initialize_hidden_state(batch_size)
+    feature_tensor=feature_tensor.long()
+    output,hidden_state=model(feature_tensor,hidden_state)
+    prediction=torch.round(output.squeeze())
+    if(prediction.item()==0):
+       resultString="{:.4f}  Positive sentence!".format(output.item())
     else:
-        resultString=resultString+"Negative statement detected."
-
+       resultString="{:.4f}  Negative sentence!".format(output.item())
     return resultString
 ########################################################################
 def createData():
-    with open('reviews.txt', 'r') as f:
-        reviews = f.read()
-    with open('labels.txt', 'r') as f:
-        labels = f.read()
-    reviews = reviews.lower() # lowercase, standardize
-    all_text = ''.join([c for c in reviews if c not in punctuation])
-    reviews_split = all_text.split('\n')
-    all_text = ' '.join(reviews_split)
-    words = all_text.split()
-    counts = Counter(words)
-    vocab = sorted(counts, key=counts.get, reverse=True)
-    vocab_to_int = {word: ii for ii, word in enumerate(vocab, 1)}
+    vocab_to_int = np.load('vocab_to_int.npy').item()
     return vocab_to_int
-
 #############################################################
-class SentimentRNN(nn.Module):
-    def __init__(self, vocab_size, output_size, embedding_dim, hidden_dim, n_layers, drop_prob=0.5):
-        super(SentimentRNN, self).__init__()
-        self.output_size = output_size
-        self.n_layers = n_layers
-        self.hidden_dim = hidden_dim
-        self.embedding = nn.Embedding(vocab_size, embedding_dim)
-        self.lstm = nn.LSTM(embedding_dim, hidden_dim, n_layers, 
-                            dropout=drop_prob, batch_first=True)
-        self.dropout = nn.Dropout(0.3)
-        self.fc = nn.Linear(hidden_dim, output_size)
-        self.sig = nn.Sigmoid()
-        
-
-    def forward(self, x, hidden):
-        
-        batch_size = x.size(0)
-        embeds = self.embedding(x)
-        lstm_out, hidden = self.lstm(embeds, hidden)
-        lstm_out = lstm_out.contiguous().view(-1, self.hidden_dim)
-        out = self.dropout(lstm_out)
-        out = self.fc(out)
-        sig_out = self.sig(out)
-        sig_out = sig_out.view(batch_size, -1)
-        sig_out = sig_out[:, -1]
-        return sig_out, hidden
-    
-    
-    def init_hidden(self, batch_size):
-        weight = next(self.parameters()).data
-        hidden = (weight.new(self.n_layers, batch_size, self.hidden_dim).zero_(),
-                  weight.new(self.n_layers, batch_size, self.hidden_dim).zero_())
-        return hidden
+class SentimentNetwork(nn.Module):
+  def __init__(self,vocabulary_size,output_size,embedding_dimension,hidden_dimension,number_of_layers,dropout_probability=0.5):
+    super(SentimentNetwork,self).__init__()
+    self.output_size=output_size
+    self.number_of_layers=number_of_layers
+    self.hidden_dimension=hidden_dimension
+    self.embedding=nn.Embedding(vocabulary_size,embedding_dimension)
+    self.lstm=nn.LSTM(embedding_dimension,hidden_dimension,number_of_layers,dropout=dropout_probability,batch_first=True)
+    self.dropout=nn.Dropout(dropout_probability)
+    self.finalLayer=nn.Linear(hidden_dimension,output_size)
+    self.sigmoid=nn.Sigmoid()
+   
+  def forward(self,x,hidden):
+    batch_size=x.size(0)
+    embedding_output=self.embedding(x)
+    lstm_output,hidden=self.lstm(embedding_output,hidden)
+    lstm_output=lstm_output.contiguous().view(-1,self.hidden_dimension)
+    output=self.dropout(lstm_output)
+    output=self.finalLayer(output)
+    sigmoid_output=self.sigmoid(output)
+    sigmoid_output=sigmoid_output.view(batch_size,-1)
+    sigmoid_output=sigmoid_output[:,-1]
+    return sigmoid_output,hidden
+  
+  def initialize_hidden_state(self,batch_size):
+    weight=next(self.parameters()).data    
+    hidden=(weight.new(self.number_of_layers,batch_size,self.hidden_dimension).zero_(),weight.new(self.number_of_layers,batch_size,self.hidden_dimension).zero_())
+    return hidden
 ############################################################################################
 def buildNetwork(vocab_to_int):
-    vocab_size = len(vocab_to_int)+1 # +1 for the 0 padding + our word tokens
-    output_size = 1
-    embedding_dim = 200
-    hidden_dim = 256
-    n_layers = 2
-    net = SentimentRNN(vocab_size, output_size, embedding_dim, hidden_dim, n_layers)
-    lr=0.003
-    criterion = nn.BCELoss()
-    optimizer = torch.optim.Adam(net.parameters(), lr=lr)
-    return net
-  
-
+  vocabulary_size=len(vocab_to_int)+1
+  output_size=1
+  embedding_dimension=200
+  hidden_dimension=256
+  number_of_layers=2
+  model=SentimentNetwork(vocabulary_size,output_size,embedding_dimension,hidden_dimension,number_of_layers)
+  learning_rate=0.003
+  criterion=nn.BCELoss()
+  optimizer=torch.optim.Adam(model.parameters(),lr=learning_rate)
+  return model
+#########################################################################################
 def load_checkpoint(filepath,vocab_to_int):
     model=buildNetwork(vocab_to_int)
     model.load_state_dict(torch.load(filepath))
+    model.cpu()
     return model
-	
 #############################################################################################
 def getSentimentPredictionResult(inputReview):
     vocab_to_int=createData()
-    model=load_checkpoint('CHECKPOINT99.pth',vocab_to_int)
+    model=load_checkpoint('SentimentAnalysisCheckpoint.pth',vocab_to_int)
     seq_length=200
     return predict(model,inputReview,vocab_to_int,seq_length)
-
-
-
-#predict(net, test_review_pos, seq_length)
-
-
-
-
-
-
-
-
-
-
-
-
-'''
-def get_model():
-	checkpoint_path='PyTorchCheckpoint'
-	checkpoint=torch.load(checkpoint_path,map_location='cpu')
-  	model=checkpoint["model"]
-  	model.load_state_dict(checkpoint['state_dict'])
-  	return model
-
- #def getFeatures():
-'''
