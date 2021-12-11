1e45551ea7937eb4c312bb66acf102487d14fb4e
Manu Gond <manugond45@gmail.com>
2021-12-11 18:37:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask,render_template,request
 import sys
 import logging
+import os
 app = Flask(__name__)
 # commons.py contains all methods to load the model from checkpoint and predict the sentiment
 from commons import *
@@ -11,12 +12,12 @@ def hello():
 		# if GET method then render the homepage where data can be entered
 		return render_template('index.html')
 	if request.method=='POST':
-		# if POST method then first call the prediction method to get result 
+		# if POST method then first call the prediction method to get result
 		# then render the result template and put result there
 		file=request.form['file']
 		predicted_result=getSentimentPredictionResult(file)
 		return render_template('result.html',sentiment=predicted_result)
-		
+
 if __name__=='__main__':
 	app.run(port=os.getenv('PORT',5000))
 	app.logger.addHandler(logging.StreamHandler(sys.stdout))
